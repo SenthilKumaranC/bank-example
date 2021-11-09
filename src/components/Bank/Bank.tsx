9d8817cdd52ee5324c5dc3f9556859e59f566b21
unknown <senthil.kumaran@gridlogic.in>
2021-11-09 09:12:32 +0530

    'Bank'

---
+++ src/components/Bank/Bank.tsx
@@ -0,0 +1,70 @@
+import { useCallback, useReducer, useRef } from 'react'
+import './Bank.css'
+
+const BANK_ACTIONS = {
+    TRANSFER: "TRANSFER",
+    DEPOSIT: "DEPOSIT"
+}
+
+
+
+export interface IBankDetails {
+    balance: number;
+    fdBalance: number;
+    statement : any[];
+}
+
+const initialBankDetails: IBankDetails = { balance: 0, fdBalance: 10000 , statement : []}
+
+const processBankCommands = (oldBankDetails: IBankDetails, action: any) => {
+
+    let newBankDetails = { ...oldBankDetails };
+
+    const {amount} = action.payload;
+
+    if (action.type === BANK_ACTIONS.DEPOSIT) {
+        newBankDetails.balance = oldBankDetails.balance + amount
+    }
+    if (action.type === BANK_ACTIONS.TRANSFER) {
+        newBankDetails.balance = oldBankDetails.balance - amount
+    }
+    return newBankDetails;
+
+}
+
+const Bank = () => {
+
+    const [state, dispatch] = useReducer(processBankCommands, initialBankDetails);
+    const transferInput = useRef<any>();
+    const depositInput = useRef<any>();
+
+    const transferMoney = useCallback(() => {
+        dispatch({
+            type: BANK_ACTIONS.TRANSFER,
+            payload: { amount: Number(transferInput.current.value) }
+        })
+        transferInput.current.value = "";
+    }, []);
+
+    const depositMoney = useCallback(() => {
+        dispatch({
+            type: BANK_ACTIONS.DEPOSIT,
+            payload: { amount: Number(depositInput.current.value) }
+        })
+        depositInput.current.value = "";
+    }, []);
+
+    return <div className="Bank">
+
+        <div>My Bank Balance : {state.balance}</div>
+
+        <input ref={transferInput} placeholder="Enter Amount" type="number"></input>
+        <button onClick={transferMoney}>Transfer</button>
+
+        <input ref={depositInput} placeholder="Enter Amount" type="number"></input>
+        <button onClick={depositMoney}>Deposit</button>
+
+    </div>
+}
+
+export default Bank
